e0f54e9ddd85de7278923ac8f52b641586c0ed1b
NguyenAnhTuan1912 <nguyenanhtuan19122002@gmail.com>
2025-01-17 16:56:22 +0700

    Update logging, update some classes, objects

--- server/src/databases/task/index.ts
+++ server/src/databases/task/index.ts
@@ -4,9 +4,17 @@ import mongoose, { Model } from "mongoose";
 // Import classes
 import { DirReader } from "src/classes/DirReader";
 
+// Import logger builder
+import { LoggerBuilder } from "src/logger";
+
 // Import configs
 import AppConfig from "src/app.config.json";
 import DatabaseConfig from "src/db.config.json";
+
+// Start log
+LoggerBuilder.Logger.info(
+  `Starting setup ORM of ${DatabaseConfig.databases[1].name}.`
+);
 
 const database = DatabaseConfig.databases[1];
 
@@ -33,6 +41,13 @@ const databaseEngine = database.engine
   ? database.engine
   : process.env[`${uppercasedDatabaseName}_ENGINE`];
 
+LoggerBuilder.Logger.info(
+  `Setup ORM of ${DatabaseConfig.databases[1].name} done. Database: ${databaseName}; Host: ${databaseHost}; Engine: ${databaseEngine}.`
+);
+LoggerBuilder.Logger.info(
+  `ORM of ${DatabaseConfig.databases[1].name} is ready to used.`
+);
+
 export type TaskManagerModelsType = {
   Task: Model<any>;
   Assignment: Model<any>;
@@ -43,12 +58,19 @@ export type TaskManagerModelsType = {
 
 const _models = {};
 
-export default async function () {
+export default function () {
   const connectionString = `mongodb://${databaseUsername}:${databasePassword}@${databaseHost}:27017/${databaseName}`;
 
-  await mongoose.connect(connectionString, {
-    authSource: "admin",
-  });
+  LoggerBuilder.Logger.info(`Try to connect to database ${databaseName}.`);
+
+  mongoose
+    .connect(connectionString, {
+      authSource: "admin",
+    })
+    .catch((error) => {
+      LoggerBuilder.Logger.error(error.message);
+      process.exit(1);
+    });
 
   if (Object.keys(_models).length === 0) {
     const modelFilePaths = reader.getAllPathsToFilesSync(rootPath);
@@ -65,7 +87,14 @@ export default async function () {
       const result = model();
       (_models as any)[result.name] = result.model;
     }
+    LoggerBuilder.Logger.info(
+      `Build model done. Total of models is ${Object.keys(_models).length}.`
+    );
   }
+
+  LoggerBuilder.Logger.info(
+    `All ORM Models of ${DatabaseConfig.databases[0].name} are ready to used.`
+  );
 
   return _models as TaskManagerModelsType;
 }
